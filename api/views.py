d613dfd418a091dd11a04add4996ca6c7ef7bf88
kowsalya2025 <kowsalyacse1992@gmail.com>
2025-09-16 23:49:07 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -2,6 +2,10 @@ from rest_framework import viewsets, filters
 from .models import MiniProject
 from .serializers import MiniProjectSerializer
 from .permissions import IsTrainerOrOwnerOrReadOnly
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
 
 class MiniProjectViewSet(viewsets.ModelViewSet):
     queryset = MiniProject.objects.all().order_by('-created_at')
@@ -11,3 +15,14 @@ class MiniProjectViewSet(viewsets.ModelViewSet):
     search_fields = ['title', 'description']
     ordering_fields = ['due_date', 'priority']
 
+class RegisterView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        if User.objects.filter(username=username).exists():
+            return Response({"detail": "Username already exists"}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        return Response({"detail": "User created successfully"}, status=status.HTTP_201_CREATED)
